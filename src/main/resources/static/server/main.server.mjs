641e43e6deed002f428e724d7dd47d9205681d32
junasaricky <junasa.ricky@gmail.com>
2025-07-10 01:56:36 +0800

    Update Angular build and Docker for deployment

--- src/main/resources/static/server/main.server.mjs
+++ src/main/resources/static/server/main.server.mjs
@@ -1,2 +1,2 @@
 import './polyfills.server.mjs';
-import{a}from"./chunk-B4EJZ3KW.mjs";import"./chunk-PPI7UDVD.mjs";import"./chunk-VVCT4QZE.mjs";export{a as default};
+import{a}from"./chunk-775N7QIW.mjs";import"./chunk-PPI7UDVD.mjs";import"./chunk-VVCT4QZE.mjs";export{a as default};
