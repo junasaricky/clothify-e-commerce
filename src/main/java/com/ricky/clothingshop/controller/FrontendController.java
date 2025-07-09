483aa402990be6c0caf615edefca42242a4601de
junasaricky <junasa.ricky@gmail.com>
2025-07-10 04:45:59 +0800

    Updated

--- src/main/java/com/ricky/clothingshop/controller/FrontendController.java
+++ src/main/java/com/ricky/clothingshop/controller/FrontendController.java
@@ -1,9 +1,9 @@
 package com.ricky.clothingshop.controller;
 
-import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.bind.annotation.GetMapping;
 
-@Controller
+@RestController
 public class FrontendController {
 
     @GetMapping(value = {
