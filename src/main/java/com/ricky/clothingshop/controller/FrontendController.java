38ab9740f64a4a62c5e3f7861d8df30ed993bff8
junasaricky <junasa.ricky@gmail.com>
2025-07-10 04:19:15 +0800

    Updated

--- src/main/java/com/ricky/clothingshop/controller/FrontendController.java
+++ src/main/java/com/ricky/clothingshop/controller/FrontendController.java
@@ -1,17 +1,14 @@
 package com.ricky.clothingshop.controller;
 
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class FrontendController {
 
-    @RequestMapping(value = {
-        "/", 
-        "/{x:[\\w\\-]+}", 
-        "/{x:^(?!api$).*$}/**/{y:[\\w\\-]+}"
-    })
-    public String forward() {
+    @GetMapping(value = {"/{path:[^\\.]*}", "/**/{path:[^\\.]*}"})
+    public String redirect() {
         return "forward:/index.html";
     }
+
 }
