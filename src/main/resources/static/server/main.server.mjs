b6dbdc79de6994032f6b716c4177bc15d3f8382e
junasaricky <junasa.ricky@gmail.com>
2025-07-13 22:28:35 +0800

    Updated Angular for login interface

--- src/main/resources/static/server/main.server.mjs
+++ src/main/resources/static/server/main.server.mjs
@@ -1,2 +1,2 @@
 import './polyfills.server.mjs';
-import{a}from"./chunk-CS6RS7WO.mjs";import"./chunk-PPI7UDVD.mjs";import"./chunk-VVCT4QZE.mjs";export{a as default};
+import{a}from"./chunk-2J3U3HEF.mjs";import"./chunk-PPI7UDVD.mjs";import"./chunk-VVCT4QZE.mjs";export{a as default};
