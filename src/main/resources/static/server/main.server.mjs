d819b1457415e8c78dc61a1a79a1175f54915bd8
junasaricky <junasa.ricky@gmail.com>
2025-07-14 21:32:24 +0800

    Updated UI spinner

--- src/main/resources/static/server/main.server.mjs
+++ src/main/resources/static/server/main.server.mjs
@@ -1,2 +1,2 @@
 import './polyfills.server.mjs';
-import{a}from"./chunk-NZOYOJ7M.mjs";import"./chunk-PPI7UDVD.mjs";import"./chunk-VVCT4QZE.mjs";export{a as default};
+import{a}from"./chunk-HPXU2PSE.mjs";import"./chunk-PPI7UDVD.mjs";import"./chunk-VVCT4QZE.mjs";export{a as default};
