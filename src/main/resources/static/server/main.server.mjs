02c2745252165733db125951188adf5cfaa9a6d9
junasaricky <junasa.ricky@gmail.com>
2025-07-14 19:04:38 +0800

    Angular address API

--- src/main/resources/static/server/main.server.mjs
+++ src/main/resources/static/server/main.server.mjs
@@ -1,2 +1,2 @@
 import './polyfills.server.mjs';
-import{a}from"./chunk-NQ72OI25.mjs";import"./chunk-PPI7UDVD.mjs";import"./chunk-VVCT4QZE.mjs";export{a as default};
+import{a}from"./chunk-YKVO3VBK.mjs";import"./chunk-PPI7UDVD.mjs";import"./chunk-VVCT4QZE.mjs";export{a as default};
