1f0d53b1bdff454aa85d50bce6cd4e1ae6432888
junasaricky <junasa.ricky@gmail.com>
2025-07-14 20:23:20 +0800

    Added spinner

--- src/main/resources/static/server/main.server.mjs
+++ src/main/resources/static/server/main.server.mjs
@@ -1,2 +1,2 @@
 import './polyfills.server.mjs';
-import{a}from"./chunk-YKVO3VBK.mjs";import"./chunk-PPI7UDVD.mjs";import"./chunk-VVCT4QZE.mjs";export{a as default};
+import{a}from"./chunk-BD7NUC53.mjs";import"./chunk-PPI7UDVD.mjs";import"./chunk-VVCT4QZE.mjs";export{a as default};
