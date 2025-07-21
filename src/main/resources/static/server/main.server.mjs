9362ddc951464d067662bdc6e7c2c7710f61e1af
junasaricky <junasa.ricky@gmail.com>
2025-07-21 18:27:00 +0800

    Updated Angular for spinner

--- src/main/resources/static/server/main.server.mjs
+++ src/main/resources/static/server/main.server.mjs
@@ -1,2 +1,2 @@
 import './polyfills.server.mjs';
-import{a}from"./chunk-5RUAOWXL.mjs";import"./chunk-26BYYQLQ.mjs";import"./chunk-VVCT4QZE.mjs";export{a as default};
+import{a}from"./chunk-BT7HRT42.mjs";import"./chunk-26BYYQLQ.mjs";import"./chunk-VVCT4QZE.mjs";export{a as default};
