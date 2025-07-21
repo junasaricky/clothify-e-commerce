b91ba0ab51f47aa19ebe75f36413ecd79ef5b908
junasaricky <junasa.ricky@gmail.com>
2025-07-21 22:44:52 +0800

    Updated Angular for error handling

--- src/main/resources/static/server/main.server.mjs
+++ src/main/resources/static/server/main.server.mjs
@@ -1,2 +1,2 @@
 import './polyfills.server.mjs';
-import{a}from"./chunk-BVFLZFRZ.mjs";import"./chunk-26BYYQLQ.mjs";import"./chunk-VVCT4QZE.mjs";export{a as default};
+import{a}from"./chunk-W7I2NRNZ.mjs";import"./chunk-26BYYQLQ.mjs";import"./chunk-VVCT4QZE.mjs";export{a as default};
